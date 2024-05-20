3f4bb7bbd59c951304c22f97e9274b402d448c89
codeboi121 <105977617+codeboi121@users.noreply.github.com>
2024-05-20 12:25:50 -0400

    Update SaleRepository.java

--- src/main/java/com/salesERP/erp/dao/SaleRepository.java
+++ src/main/java/com/salesERP/erp/dao/SaleRepository.java
@@ -8,7 +8,7 @@ import com.salesERP.erp.entity.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Integer> {
 
-	// that's it ... no need to write any code LOL!
+	
 	
 	// search by name
 //	public List<Sale> findByCustomerNameContainsAllIgnoreCase(String name);
