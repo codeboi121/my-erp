9cc6cbf9791c962c00a34b96ccb695f4e3ac9674
codeboi121 <105977617+codeboi121@users.noreply.github.com>
2024-05-20 12:25:34 -0400

    Update SupplierRepository.java

--- src/main/java/com/salesERP/erp/dao/SupplierRepository.java
+++ src/main/java/com/salesERP/erp/dao/SupplierRepository.java
@@ -7,9 +7,6 @@ import com.salesERP.erp.entity.Supplier;
 
 
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
-
-
-	// that's it ... no need to write any code LOL!
 	
 	// search by name
 //	public List<Supplier> findByCustomerNameContainsAllIgnoreCase(String name);
